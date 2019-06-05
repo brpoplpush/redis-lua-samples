87c02c658f68ee2b1fec4dfc289a522ac8e4ccf5
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-06-05 21:50:56 +0200

    redis lua json example (#1)
    * redis lua json first example
    
    * discourage redis cjson lua scripts

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ using lua with redis.
 ```sh
 $ redis-cli --ldb --eval scripts/debug.lua
 ```
+
+## Redis lua and json
+
+<https://www.kyne.com.au/~mark/software/lua-cjson-manual.html>
+
+```sh
+$ redis-cli --eval scripts/json.lua
+1) "[1]"
+2) "{\"a\":\"oof\"}"
+3) "a"
+```
+
+The limitations of `cjson` within the redis should discourage
+you from using it for redis lua scripting.
---
+++ scripts/json.lua
@@ -0,0 +1,34 @@
+-- https://www.kyne.com.au/~mark/software/lua-cjson-manual.html
+-- local mykey = KEYS[1]
+
+-- the original purpose of this script was to provide
+-- a two-way serialization JSON string to Redis Hash and back
+
+local json_text = '{ "foo": "a", "bar": {"a": "oof"}, "baz": [1] }'
+local parsed = cjson.decode(json_text)
+
+-- return parsed
+-- (empty list or set)
+-- Known limitation of tables parsed via cjson
+-- https://stackoverflow.com/questions/51710571/cjson-decode-in-lua-returns-array-of-empty-arrays-redis-cli
+
+if type(parsed) == 'table' then
+  local keys = {}
+  local values = {}
+  local index = 1
+
+  for k,v in pairs(parsed) do
+    keys[index] = k
+    if type(v) == 'table' then
+      values[index] = cjson.encode(v) -- or try just: v
+    else
+      values[index] = v
+    end
+    index = index + 1
+  end
+
+  return values
+
+else
+  return -1
+end
